c0635e6c1465b9c926ed3782519ecb8a43b4fb3a
MateusResendeR <mateus.resende.r@gmail.com>
2025-01-28 11:48:40 -0300

    Add refresh

--- src/pages/Journey/index.tsx
+++ src/pages/Journey/index.tsx
@@ -59,7 +59,7 @@ const Journey = ({token, setToken}: JourneyProps) => {
       <>
         <Header name={name} email = {email}/>
         <div className="container mx-auto p-4">
-          <div className="flex justify-end mb-4">
+          <div className="flex justify-end mb-4 gap-4">
             <button onClick={() => setModalOpen(true)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
               Nova Jornada
             </button>
