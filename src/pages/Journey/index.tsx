ae6e2f82d6a302bbfd6f6c8a9a17fcacddce8c94
MateusResendeR <mateus.resende.r@gmail.com>
2025-01-28 11:44:28 -0300

    Add status

--- src/pages/Journey/index.tsx
+++ src/pages/Journey/index.tsx
@@ -64,6 +64,9 @@ const Journey = ({token, setToken}: JourneyProps) => {
             <button onClick={() => setModalOpen(true)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
               Nova Jornada
             </button>
+            <button onClick={() => getJourneys()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Atualizar lista
+            </button>
           </div>
           <div>
             <div className="overflow-x-auto">
